ebb56f48b40b43c00e83091bb9331ad814c33516
Jared Herring <jaredherring@Jareds-MacBook-Air.local>
2026-04-29 10:20:10 -0500

    changed to /finalfreight/

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',
+  base: '/FinalFreight/',
   plugins: [react(), tailwindcss()],
 })
